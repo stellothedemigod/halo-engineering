f867526c9dfc83aff90c09665e22eb87a55af055
stellothedemigod <divineodigili@gmail.com>
2022-07-30 20:56:17 +0100

    messed up the code

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ menubar.onclick = () =>{
     menubar.classList.toggle('fa-times');
     mynav.classList.toggle('active');
 }
+
+// let clients = document.querySelector('.our-clients');
+// let clientlist = document.querySelector('.main-client')
+
+
+
+// clients.onclick = () =>{
+//     clients.classList.toggle('fa-times');
+//     clientlist.classList.toggle('active');
+// }
+
+// let btn = document.querySelector('#clients')
+// let div = document.querySelector('.main-client')
+
+// btn.addEventListener('click', () =>{
+//     if( class client{style.display} === 'none'){
+//         class client{style.display} = 'block'
+         
+//     }else {
+        
+//     }
+// })
