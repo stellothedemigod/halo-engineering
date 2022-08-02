ae40c5a5eb25661ca883b803efcd4ec27216b05d
stellothedemigod <divineodigili@gmail.com>
2022-08-02 22:24:11 +0100

    added show more and show less buttons on what we do section

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ function hide () {
     document.getElementById('our_clients').style.display= "none"
     document.getElementById('hide').style.display="block"
 }
+
+
+
+function show() {
+    document.getElementById('more').style.display="block"
+    document.getElementById('we-do-button').style.display="none"
+}
+
+function minimize () {
+    document.getElementById('more').style.display= "none"
+    document.getElementById('we-do-button').style.display=""
+}
