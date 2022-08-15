d24458c6709bc14e560cec9c774367ec6407fc01
stellothedemigod <divineodigili@gmail.com>
2022-08-15 21:32:29 +0100

    removed unused js and adjusted html

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// let menubar = document.querySelector('#menu-bars');
-// let mynav = document.querySelector('.navbar')
 
-
-
-// menubar.onclick = () =>{
-//     menubar.classList.toggle('fa-times');
-//     mynav.classList.toggle('active');
-// }
 
 
 
